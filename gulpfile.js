222c2e2e86475c81f78b8bae6e7fd5fa673c1c1d
jacobcheatley <jacobcheatley@gmail.com>
2017-04-30 16:24:45 +1200

    File watchers

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
+var gulp = require('gulp'),
+    pug = require('gulp-pug'),
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css');
+
 
 gulp.task('debug', function() {
    console.log('Bugs totally removed.');
@@ -10,19 +11,23 @@ gulp.task('debug', function() {
 gulp.task('pug', function() {
     gulp.src(['src/*.pug'])
         .pipe(pug())
-        .pipe(gulp.dest('build'))
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('styles', function() {
     gulp.src(['src/style/*.scss'])
         .pipe(sass())
         .pipe(cleanCSS())
-        .pipe(gulp.dest('build/style'))
+        .pipe(gulp.dest('build/style'));
 });
 
 gulp.task('static', function() {
     gulp.src('src/static/*')
-        .pipe(gulp.dest('build/static'))
+        .pipe(gulp.dest('build/static'));
 });
 
-gulp.task('default', ['pug', 'styles', 'static']);
+gulp.task('default', ['pug', 'styles', 'static'], function () {
+    gulp.watch('**/*.pug', ['pug']);
+    gulp.watch('**/*.scss', ['styles']);
+    gulp.watch('src/static/*', ['static']);
+});
